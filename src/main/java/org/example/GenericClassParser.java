ada3aac4532d6f7d933e5fb38f949d08f2d874f7
ysferdgnn <ysferdgnn@gmail.com>
2021-08-11 17:41:17 +0300

    Upgraded Junit lib added BigDecimal parse feature added Long parse feature added BigDecimalParse tests added Long Parse test

--- src/main/java/org/example/GenericClassParser.java
+++ src/main/java/org/example/GenericClassParser.java
@@ -2,6 +2,7 @@ package org.example;
 
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -66,34 +67,7 @@ public class GenericClassParser<T,V> {
                 logger.info(String.format("Expected Value -> %s",expectedValue));
                 /* begin Integer*/
                 if (Integer.class.equals(destinationClassType)) {
-                    Integer value ;
-
-                    if(isNumeric(expectedValue)){
-                        try{
-                            if (isContainsDotOrComma(expectedValue)){
-                                expectedValue = clearDotsAndCommasFromString(expectedValue);
-                                Double doubleValue = Double.valueOf(expectedValue);
-                                value = doubleValue.intValue();
-                            }else{
-                                value = Integer.valueOf(expectedValue);
-                            }
-
-
-
-
-
-                        }catch (NumberFormatException e){
-                            value =0;
-                        }
-                    }else{
-                        value=0;
-                    }
-
-
-
-
-
-
+                    Integer value = parseInteger(expectedValue);
 
                     destinationField.set(destinationClazz, value);
                 }
@@ -107,14 +81,15 @@ public class GenericClassParser<T,V> {
                 /* end String*/
                 /* start BigDecimal*/
                 else if (BigDecimal.class.equals(destinationClassType)){
-                    BigDecimal value = new BigDecimal(sourceMap.get(sourceField));
-                    destinationField.set(destinationClazz,value);
+                    BigDecimal value = parseBigDecimal(expectedValue);
+                    destinationField.set(destinationClazz, value);
+
                 }
                 /* end BigDecimal*/
 
                 /* start Long */
                 else if (Long.class.equals(destinationClassType)){
-                    Long value = Long.valueOf(sourceMap.get(sourceField));
+                    Long value = parseLong(expectedValue);
                     destinationField.set(destinationClazz,value);
                 }
                 /* end Long */
@@ -142,7 +117,7 @@ public class GenericClassParser<T,V> {
 
                 /* start Character */
                 else if (Character.class.equals(destinationClassType)) {
-                    Character value = Character.valueOf(sourceMap.get(sourceField).charAt(0));
+                    Character value = sourceMap.get(sourceField).charAt(0);
                     destinationField.set(destinationClazz,value);
                 }
                 /* end Character */
@@ -164,6 +139,83 @@ public class GenericClassParser<T,V> {
 
     }
 
+    private Long parseLong(String expectedValue) {
+        Long value =0L;
+        if(isNumeric(expectedValue)){
+            if(isContainsDotOrComma(expectedValue)){
+                expectedValue =clearDotsAndCommasFromString(expectedValue);
+                if(isContainsDotOrComma(expectedValue)){
+                    logger.info(String.format("Value: %s is contains dot",expectedValue));
+                    expectedValue= expectedValue.split("\\.")[0];
+                    logger.info(String.format("Value %s changed.",expectedValue));
+                }
+
+            }
+            try{
+                logger.info(String.format("Value: %s is expected value",expectedValue));
+
+                value= Long.parseLong(expectedValue,10);
+            }catch (NumberFormatException exception){
+                logger.warning(String.format("Value: %s Long Number Format Exception",value));
+                value=0L;
+            }
+
+        }else {
+            logger.info("Value: %s is not Numeric!");
+            value=0L;
+        }
+        return value;
+    }
+
+    private BigDecimal parseBigDecimal(String expectedValue) {
+        BigDecimal value =BigDecimal.ZERO;
+        if(isNumeric(expectedValue)){
+            try {
+                if(isContainsDotOrComma(expectedValue)){
+                    expectedValue =clearDotsAndCommasFromString(expectedValue);
+
+                    value=new BigDecimal(expectedValue);
+                    logger.info(String.format("Value: %s has been parsed BigDecimal",value));
+                }else {
+                    value = new BigDecimal(expectedValue);
+                }
+
+
+            }catch (NumberFormatException exception){
+                logger.warning(String.format("Value: %s BigDecimal  Number Format Exception", expectedValue));
+                value=BigDecimal.ZERO;
+            }
+
+        }else{
+            logger.info(String.format("Value: %s is not Numeric!", expectedValue));
+        }
+        return value;
+    }
+
+    private Integer parseInteger(String expectedValue) {
+        Integer value =0;
+
+        if(isNumeric(expectedValue)){
+            try{
+                if (isContainsDotOrComma(expectedValue)){
+                    expectedValue = clearDotsAndCommasFromString(expectedValue);
+                    Double doubleValue = Double.valueOf(expectedValue);
+                    value = doubleValue.intValue();
+                }else{
+                    value = Integer.valueOf(expectedValue);
+                }
+
+            }catch (NumberFormatException e){
+                logger.warning(String.format("Value: %s Integer Number Format Exception", expectedValue));
+                value =0;
+            }
+        }else{
+            logger.info(String.format("Value: %s is not Numeric!", expectedValue));
+            value=0;
+        }
+        return value;
+    }
+
     public  Boolean isNumeric(String value){
        if(value.matches("[a-zA-Z]")){
           return  false;
@@ -171,27 +223,34 @@ public class GenericClassParser<T,V> {
        return  true;
     }
     public Boolean isContainsDotOrComma(String value){
-        logger.info(String.format("Value %s containing dot or comma",value));
+        logger.info(String.format("Value: %s containing dot or comma",value));
         if(value.contains(".") || value.contains(",")){
             return true;
         }
         return  false;
     }
     public String changeCommasAsDot(String value){
-        logger.info(String.format("Value %s changing commas as dot..",value));
+        logger.info(String.format("Value: %s changing commas as dot..",value));
         value = value.replace(',','.');
+        logger.info(String.format("Value: %s changed commas as dots",value));
         return  value;
+    }
+    public String changeDotsAsComma(String value){
+        logger.info(String.format("Value: %s changing dots as comma..",value));
+        value=value.replace(".",",");
+        logger.info(String.format("Value: %s changed dots as commas",value));
+        return value;
     }
     public int calculateDotCount(String value){
         value = changeCommasAsDot(value);
        int commaCount = (int)value.codePoints().filter(s->s =='.').count();
 
-        logger.info(String.format("Value %s containing %s comma",value,commaCount));
+        logger.info(String.format("Value: %s containing %s comma",value,commaCount));
 
        return  commaCount;
     }
     public String clearDotsAndCommasFromString(String value){
-        logger.info(String.format("Value %s clearing commas",value));
+        logger.info(String.format("Value: %s clearing commas",value));
         value = changeCommasAsDot(value);
         int dotCount = calculateDotCount(value);
         int changedDotCount =0;
@@ -207,8 +266,10 @@ public class GenericClassParser<T,V> {
 
             }
         }
-        logger.info(String.format("Value %s has been cleared.",value));
+        logger.info(String.format("Value: %s has been cleared.",value));
         return  value;
 
     }
+
+
 }
